e84b6a0218130d9c8f24c20f518fcc7eb4333e80
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-04 17:04:13 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
+
+  def build_network(options = {})
+    network = Network.find_or_create_by(options)
+    network.shows << self
+
+    network
+  end
 end
