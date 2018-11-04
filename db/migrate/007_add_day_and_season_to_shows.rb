62b938ceeda8f6869697f6b638a6bf5c10919f9f
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-04 17:10:12 +0000

    automatically backed up by learn

--- db/migrate/007_add_day_and_season_to_shows.rb
+++ db/migrate/007_add_day_and_season_to_shows.rb
@@ -2,5 +2,6 @@ class AddDayAndSeasonToShows < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
+    add_column :shows, :genre, :string
   end
 end
