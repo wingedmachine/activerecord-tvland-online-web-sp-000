fc55639a06ed6536c6f6deda24ddfd82c82d3d27
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-04 17:10:31 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{name} always says: #{catchphrase}"
+  end
+
+  def build_show(options = {})
+    show = Show.find_or_create_by(options)
+    show.characters << self
+
+    show
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(options = {})
+    network = Network.find_or_create_by(options)
+    network.shows << self
+
+    network
+  end
 end
